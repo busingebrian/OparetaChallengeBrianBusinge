0694361d4962145a6acb954b8d054f66cda610a6
bbusinge <donbrayo91@gmail.com>
2022-08-25 19:19:22 +0300

    Added sonarqube for static code analysis reports

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,7 @@
 
-o
-?app/src/main/java/com/leroy/oparetachallenge/ui/MainActivity.kt,7/d/7d60266d613f826fdd8946757275d713639e4253
 €
 Papp/src/main/java/com/leroy/oparetachallenge/repository/ConversionsRepository.kt,b/1/b17c596eeffb998fdf3289fd8c3b768cb87c1587
 
 Oapp/src/main/java/com/leroy/oparetachallenge/repository/NetworkBoundResource.kt,0/8/083eada356e3cf24519a361c748587dce3266d47
+w
+Gapp/src/main/java/com/leroy/oparetachallenge/databases/ConversionsDb.kt,8/f/8fe0aa3b35466dfd8a86e47635ce2cb2c29fa82a
---
+++ app/src/androidTest/java/com/leroy/oparetachallenge/ui/MainActivityTest.kt
@@ -0,0 +1,110 @@
+package com.leroy.oparetachallenge.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.leroy.oparetachallenge.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val appCompatTextView = onView(
+            allOf(
+                withId(R.id.fiat_currency_btn), withText("UGX"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatTextView.perform(click())
+
+        val materialTextView = onView(
+            allOf(
+                withId(androidx.appcompat.R.id.title), withText("USD"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(androidx.appcompat.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        materialTextView.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.amount_text),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("5"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.amount_text), withText("5"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(pressImeActionButton())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
